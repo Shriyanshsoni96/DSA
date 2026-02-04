597e06bb9abd622e12a7fb68a38f7a29d12538da
Shriyanshsoni96 <sonishriyansh9977@gmail.com>
2026-02-04 14:23:33 +0530

    decimaltobinary

---
+++ decimal_to_binary.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int Binary(int n )
+{
+    while(n>0)
+    {
+        
+    }
+}
+int main() {
+int i = 1;
+int n;
+cin>>n;
+cout<<"Binary No are "<<Binary(n);
+}
