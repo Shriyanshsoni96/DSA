da8e7cf6ae77891211e21198747b350253a58507
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-02-19 23:08:23 +0530

    leetcode questions

---
+++ Leetcode/724.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int pivot(int nums)
+{
+int s = sizeof(nums)/sizeof(int);
+int total=0;
+int i =0;
+for(i =0; i<s; i++ )
+{
+    total+=nums[i];
+}
+int leftsum=0;
+for( int j =0 ; j<s;j++)
+{
+    int rightsum=total-leftsum-nums[j];
+    if(leftsum==rightsum)
+    {
+        cout<<j;
+    }
+    leftsum+=nums[j];
+}
+return -1;
+}
+
+int main(){
+int nums[]={1,7,3,6,5,6};
+cout<<pivot(nums);
+}
---
+++ maths fucntions/kadens_joseph.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+// leetcode 53
+66
+26
+27
+return 0;
+}
