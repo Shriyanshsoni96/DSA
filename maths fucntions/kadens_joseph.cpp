d1b98d8a518f51758e7570139b4ab5720fb76ed2
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-02-26 22:16:36 +0530

    Kadanes algorithm of maximum no of subarrays

--- maths fucntions/kadens_joseph.cpp
+++ maths fucntions/kadens_joseph.cpp
@@ -1,9 +1,24 @@
 #include<iostream>
+#include<vector>
 using namespace std;
+   int maxSubArray(vector<int>& nums) {
+   int csum=0;
+     int max=nums[0];
+     for(int i = 0 ; i<nums.size(); i++)
+     {
+        csum+=nums[i];
+        if(csum>max)
+        {
+            max=csum;
+        }
+        if(csum<0)
+        {
+            csum=0;
+        }
+     }   
+     return max;
+    }
 int main(){
-// leetcode 53
-66
-26
-27
+
 return 0;
 }
