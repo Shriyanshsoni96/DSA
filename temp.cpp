3148d5955547916a7cab9616e500db72c9dbd02e
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-02-26 23:36:45 +0530

    vectors basics

--- temp.cpp
+++ temp.cpp
@@ -1,19 +1,39 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 int main(){
-int n[]={1,2,3,4};
+    //vector<int>arr(3,0);
+    //3 is the size of vector and we store all the element as zero
+    // Its a basic type to print the vector and array;
+    // for(int i = 0 ; i < arr.size();i++)
+    // {
+    // cout<<arr[i]<<" ";
+    // }
+    
+    //For each loop in this i store the value of vec 
+    // datatype of this i in iteration is depend on the vectors datatype
+    vector<int> arr;
+   
+    arr.push_back(23);
+     for(int i : arr)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl<<arr.size()<<endl;
+    arr.push_back(34);
+    arr.push_back(45);
+    arr.push_back(67);
+    arr.push_back(78);
 
- for(int i = 0 ; i<4;i++)
- {int pro=1;
-     for(int j = 0 ; j<4 ; j++)
-     {
-        if(i!=j)
-        {
-            pro=pro*n[j];
-        }
-     }
-     cout<<pro<<" ";
- }
- 
-return 0;
+     arr.pop_back();
+    cout<<endl; 
+     for(int i : arr)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl<<arr.front();
+    cout<<endl<<arr.back();
+
+
+
 }
