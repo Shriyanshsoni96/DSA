5c4b3017ab3bd3a89443fc21b66e6dee2a59c844
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-02-27 19:23:49 +0530

    basics of 2d arrays

--- Arrays/2D__Arrays/intialize.cpp
+++ Arrays/2D__Arrays/intialize.cpp
@@ -1,26 +1,43 @@
 #include<iostream>
 using namespace std;
 int main(){
-int arr[][4]={{1,2,3,4},
-             {4,5,6,7},
-            {8,9,10,11}
-        };
+int arr[3][4]={{1,2,3,4},{4,5,6,7},{8,9,10,11}};
 
- int g = sizeof(arr)/(sizeof(int)*sizeof(int));
- int h =sizeof(arr)/sizeof(int);
+int g = sizeof(arr)/(sizeof(int)*sizeof(int));
 
-int a[2][3]={1,2,3,4,5};
-int m , n ;
-cin>>m>>n;
-int b[m][n]={0};
-for(int i = 0 ; i<m;i++)
+int h =sizeof(arr)/sizeof(int);
+
+int a[3][4];
+int row=3,clm=4;
+  
+for(int i = 0 ; i < row ; i++)
 {
-    for(int j = 0 ; j<n ;j++)
+    for(int j = 0 ; j < clm ;j++ )
     {
-        cout<<b[i][j]<<"  ";
+        cin>>a[i][j];
+    }
+}
+cout<<endl;
+for(int i = 0 ; i < row ; i++)
+{
+    for(int j = 0 ; j < clm ;j++ )
+    {
+        cout<<a[i][j]<<" ";
     }
     cout<<endl;
 }
+// cin>>m>>n;
+// int b[m][n]={0};
+
+
+// for(int i = 0 ; i<3;i++)//no of rows
+// {
+//     for(int j = 0 ; j<4 ;j++)//no of coloumns 
+//     {
+//         cout<<arr[i][j]<<"  ";
+//     }
+//     cout<<endl;
+// }
 
 return 0;
 }
---
+++ Arrays/2D__Arrays/tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+
+// for(int i = 0 ; i<3;i++)//no of rows
+// {
+//     for(int j = 0 ; j<4 ;j++)//no of coloumns 
+//     {
+//         cout<<arr[i][j]<<"  ";
+//     }
+//     cout<<endl;
+// }
