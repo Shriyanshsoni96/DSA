bef107046ca6a3af451ac3964e8dd5f3858f8789
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-03-24 10:17:43 +0530

    daigonal

--- Arrays/2D__Arrays/Diagonal_travesing.cpp
+++ Arrays/2D__Arrays/Diagonal_travesing.cpp
@@ -7,41 +7,31 @@ vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
     int m = mat.size();
     int n = mat[0].size();
     
-    vector<int> result;
-    
+    vector<int> ans;
+
     for (int d = 0; d < m + n - 1; d++) {
         vector<int> temp;
-        
+
         int row = (d < n) ? 0 : d - n + 1;
         int col = (d < n) ? d : n - 1;
-        
-        // collect elements of this diagonal
+
+        // collect diagonal
         while (row < m && col >= 0) {
             temp.push_back(mat[row][col]);
             row++;
             col--;
         }
-        
-        // reverse for even diagonals
+
+        // reverse for zig-zag
         if (d % 2 == 0) {
             reverse(temp.begin(), temp.end());
         }
-        
-        // add to result
+
+        // add to answer
         for (int x : temp) {
-            result.push_back(x);
+            ans.push_back(x);
         }
     }
-    
-    return result;
-}
 
-int main() {
-    vector<vector<int>> mat = {{1,2,3},{4,5,6},{7,8,9}};
-    
-    vector<int> ans = findDiagonalOrder(mat);
-    
-    for (int x : ans) {
-        cout << x << " ";
-    }
+    return ans;
 }
