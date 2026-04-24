3444172ba94df89661891f1d5a9c346bd87c440c
Shriyansh Soni <sonishriyansh9977@gmail.com>
2026-04-24 14:42:00 +0530

    Checking that array is sorted or not

--- recursion/array/sorted_check.cpp
+++ recursion/array/sorted_check.cpp
@@ -7,18 +7,15 @@ bool isSort(int num[], int size){
     if(size == 1 or size == 0){      // base case
         return true;
     }
-
     if(num[0] < num[1] and isSort(num + 1, size - 1)){ // recursive function
         return true;
     }
-
     return false;
 }
 
 int main(){
 int num[] = {1,2,3,4,0};
 cout<< boolalpha << isSort(num, 5);
-
 return 0;
 }
 
